bcb1aff3a06271eb1241b8bd4149083eaf64355f
grandeto <grandeto.com@gmail.com>
2022-06-07 16:09:53 +0300

    f/1 - update tests

--- scrub_test.go
+++ scrub_test.go
@@ -2,6 +2,8 @@ package scrub
 
 import (
 	"encoding/json"
+	"encoding/xml"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -22,14 +24,17 @@ type Users struct {
 	UserInfo []User
 }
 
-// TestScrubSimple tests scrubbing on a simple struct with default
-// sensitive fields.
-func TestScrubSimple(t *testing.T) {
+// Tests json format scrubbing on a simple struct with default options.
+func TestScrubSimpleFixedLenJson(t *testing.T) {
+	MaskLenVary = false
+
 	user := &User{
 		Username:  "Shyam Rathi",
 		Password:  "nutanix/4u",
 		DbSecrets: []string{"db_secret_1", "db_secret_2"},
 	}
+
+	empty := &User{}
 
 	userScrubbed := &User{
 		Username:  "Shyam Rathi",
@@ -37,12 +42,76 @@ func TestScrubSimple(t *testing.T) {
 		DbSecrets: []string{"db_secret_1", "db_secret_2"},
 	}
 
-	validateScrub(t, user, userScrubbed, nil)
+	validateScrub(t, empty, user, userScrubbed, nil, JsonScrub)
 }
 
-// TestScrubNested tests scrubbing on a nested complex struct with
-// specified sensitive fields.
-func TestScrubNested(t *testing.T) {
+// Tests xml format scrubbing on a simple struct with default options.
+func TestScrubSimpleFixedLenXml(t *testing.T) {
+	MaskLenVary = false
+
+	user := &User{
+		Username:  "Shyam Rathi",
+		Password:  "nutanix/4u",
+		DbSecrets: []string{"db_secret_1", "db_secret_2"},
+	}
+
+	empty := &User{}
+
+	userScrubbed := &User{
+		Username:  "Shyam Rathi",
+		Password:  "********",
+		DbSecrets: []string{"db_secret_1", "db_secret_2"},
+	}
+
+	validateScrub(t, empty, user, userScrubbed, nil, XmlScrub)
+}
+
+// Tests json format scrubbing on a simple struct with default options and varying mask length.
+func TestScrubSimpleVaryLenJson(t *testing.T) {
+	MaskLenVary = true
+
+	user := &User{
+		Username:  "Shyam Rathi",
+		Password:  "nutanix/4u",
+		DbSecrets: []string{"db_secret_1", "db_secret_2"},
+	}
+
+	empty := &User{}
+
+	userScrubbed := &User{
+		Username:  "Shyam Rathi",
+		Password:  strings.Repeat("*", len(user.Password)),
+		DbSecrets: []string{"db_secret_1", "db_secret_2"},
+	}
+
+	validateScrub(t, empty, user, userScrubbed, nil, JsonScrub)
+}
+
+// Tests xml format scrubbing on a simple struct with default options and varying mask length.
+func TestScrubSimpleVaryLenXml(t *testing.T) {
+	MaskLenVary = true
+
+	user := &User{
+		Username:  "Shyam Rathi",
+		Password:  "nutanix/4u",
+		DbSecrets: []string{"db_secret_1", "db_secret_2"},
+	}
+
+	empty := &User{}
+
+	userScrubbed := &User{
+		Username:  "Shyam Rathi",
+		Password:  strings.Repeat("*", len(user.Password)),
+		DbSecrets: []string{"db_secret_1", "db_secret_2"},
+	}
+
+	validateScrub(t, empty, user, userScrubbed, nil, XmlScrub)
+}
+
+// Tests json format scrubbing on a nested complex struct with specific sensitive fields.
+func TestScrubNestedFixedLenJson(t *testing.T) {
+	MaskLenVary = false
+
 	users := &Users{
 		Secret: "secret_sshhh",
 		Keys:   []string{"key_1", "key_2", "key_3"},
@@ -60,9 +129,11 @@ func TestScrubNested(t *testing.T) {
 		},
 	}
 
+	empty := &Users{}
+
 	userScrubbed := &Users{
 		Secret: "********",
-		Keys:   []string{"********", "********", "********"},
+		Keys:   []string{"........", "........", "........"},
 		UserInfo: []User{
 			{
 				Username:  "John Doe",
@@ -77,18 +148,193 @@ func TestScrubNested(t *testing.T) {
 		},
 	}
 
-	secretFields := map[string]bool{
-		"password": true, "keys": true, "secret": true, "dbsecrets": true}
-	validateScrub(t, users, userScrubbed, secretFields)
+	secretFields := map[string]map[string]string{
+		"password": make(map[string]string),
+		"keys": make(map[string]string),
+		"secret": make(map[string]string),
+		"dbsecrets": make(map[string]string),
+	}
+	secretFields["password"]["symbol"] = "*"
+	secretFields["keys"]["symbol"] = "."
+	secretFields["secret"]["symbol"] = "*"
+	secretFields["dbsecrets"]["symbol"] = "*"
+
+	validateScrub(t, empty, users, userScrubbed, secretFields, JsonScrub)
 }
 
-// TestScrubNil tests scrubbing on a empty or nil input.
-func TestScrubNil(t *testing.T) {
+// Tests xml format scrubbing on a nested complex struct with specific sensitive fields.
+func TestScrubNestedFixedLenXml(t *testing.T) {
+	MaskLenVary = false
+
+	users := &Users{
+		Secret: "secret_sshhh",
+		Keys:   []string{"key_1", "key_2", "key_3"},
+		UserInfo: []User{
+			{
+				Username:  "John Doe",
+				Password:  "John_Doe's_Password",
+				DbSecrets: []string{"John's_db_secret_1", "John's_db_secret_2"},
+			},
+			{
+				Username:  "Jane Doe",
+				Password:  "Jane_Doe's_Password",
+				DbSecrets: []string{"Jane's_db_secret_1", "Jane's_db_secret_2"},
+			},
+		},
+	}
+
+	empty := &Users{}
+
+	userScrubbed := &Users{
+		Secret: "********",
+		Keys:   []string{"........", "........", "........"},
+		UserInfo: []User{
+			{
+				Username:  "John Doe",
+				Password:  "********",
+				DbSecrets: []string{"********", "********"},
+			},
+			{
+				Username:  "Jane Doe",
+				Password:  "********",
+				DbSecrets: []string{"********", "********"},
+			},
+		},
+	}
+
+	secretFields := map[string]map[string]string{
+		"password": make(map[string]string),
+		"keys": make(map[string]string),
+		"secret": make(map[string]string),
+		"dbsecrets": make(map[string]string),
+	}
+	secretFields["password"]["symbol"] = "*"
+	secretFields["keys"]["symbol"] = "."
+	secretFields["secret"]["symbol"] = "*"
+	secretFields["dbsecrets"]["symbol"] = "*"
+
+	validateScrub(t, empty, users, userScrubbed, secretFields, XmlScrub)
+}
+
+// Tests json format scrubbing on a nested complex struct with specific sensitive fields.
+func TestScrubNestedVaryLenJson(t *testing.T) {
+	MaskLenVary = true
+
+	users := &Users{
+		Secret: "secret_sshhh",
+		Keys:   []string{"key_1", "key_2", "key_3"},
+		UserInfo: []User{
+			{
+				Username:  "John Doe",
+				Password:  "John_Doe's_Password",
+				DbSecrets: []string{"John's_db_secret_1", "John's_db_secret_2"},
+			},
+			{
+				Username:  "Jane Doe",
+				Password:  "Jane_Doe's_Password",
+				DbSecrets: []string{"Jane's_db_secret_1", "Jane's_db_secret_2"},
+			},
+		},
+	}
+
+	empty := &Users{}
+
+	userScrubbed := &Users{
+		Secret: "************",
+		Keys:   []string{".....", ".....", "....."},
+		UserInfo: []User{
+			{
+				Username:  "John Doe",
+				Password:  "*******************",
+				DbSecrets: []string{"******************", "******************"},
+			},
+			{
+				Username:  "Jane Doe",
+				Password:  "*******************",
+				DbSecrets: []string{"******************", "******************"},
+			},
+		},
+	}
+
+	secretFields := map[string]map[string]string{
+		"password": make(map[string]string),
+		"keys": make(map[string]string),
+		"secret": make(map[string]string),
+		"dbsecrets": make(map[string]string),
+	}
+	secretFields["password"]["symbol"] = "*"
+	secretFields["keys"]["symbol"] = "."
+	secretFields["secret"]["symbol"] = "*"
+	secretFields["dbsecrets"]["symbol"] = "*"
+
+	validateScrub(t, empty, users, userScrubbed, secretFields, JsonScrub)
+}
+
+// Tests xml format scrubbing on a nested complex struct with specified sensitive fields.
+func TestScrubNestedVaryLenXml(t *testing.T) {
+	MaskLenVary = true
+
+	users := &Users{
+		Secret: "secret_sshhh",
+		Keys:   []string{"key_1", "key_2", "key_3"},
+		UserInfo: []User{
+			{
+				Username:  "John Doe",
+				Password:  "John_Doe's_Password",
+				DbSecrets: []string{"John's_db_secret_1", "John's_db_secret_2"},
+			},
+			{
+				Username:  "Jane Doe",
+				Password:  "Jane_Doe's_Password",
+				DbSecrets: []string{"Jane's_db_secret_1", "Jane's_db_secret_2"},
+			},
+		},
+	}
+
+	empty := &Users{}
+
+	userScrubbed := &Users{
+		Secret: "************",
+		Keys:   []string{".....", ".....", "....."},
+		UserInfo: []User{
+			{
+				Username:  "John Doe",
+				Password:  "*******************",
+				DbSecrets: []string{"******************", "******************"},
+			},
+			{
+				Username:  "Jane Doe",
+				Password:  "*******************",
+				DbSecrets: []string{"******************", "******************"},
+			},
+		},
+	}
+
+	secretFields := map[string]map[string]string{
+		"password": make(map[string]string),
+		"keys": make(map[string]string),
+		"secret": make(map[string]string),
+		"dbsecrets": make(map[string]string),
+	}
+	secretFields["password"]["symbol"] = "*"
+	secretFields["keys"]["symbol"] = "."
+	secretFields["secret"]["symbol"] = "*"
+	secretFields["dbsecrets"]["symbol"] = "*"
+
+	validateScrub(t, empty, users, userScrubbed, secretFields, XmlScrub)
+}
+
+// Tests json format scrubbing on a empty or nil input.
+func TestScrubNilJson(t *testing.T) {
+	MaskLenVary = false
+
 	user := &User{
 		Username:  "",
 		Password:  "nutanix/4u",
 		DbSecrets: []string{},
 	}
+
+	empty := &User{}
 
 	userScrubbed := &User{
 		Username:  "",
@@ -97,19 +343,49 @@ func TestScrubNil(t *testing.T) {
 	}
 
 	// Validate input with empty fields
-	validateScrub(t, user, userScrubbed, nil)
+	validateScrub(t, empty, user, userScrubbed, nil, JsonScrub)
 
 	// Validate empty pointer input
 	var userEmpty *User
-	validateScrub(t, userEmpty, userEmpty, nil)
+	validateScrub(t, empty, userEmpty, userEmpty, nil, JsonScrub)
 
 	// Validate nil input
-	validateScrub(t, nil, nil, nil)
+	validateScrub(t, empty, nil, nil, nil, JsonScrub)
 }
 
-// TestScrubNestedNil tests scrubbing on a nested complex struct with
-// some nil, empty and specified sensitive fields.
-func TestScrubNestedNil(t *testing.T) {
+// Tests xml format scrubbing on a empty or nil input.
+func TestScrubNilXml(t *testing.T) {
+	MaskLenVary = false
+
+	user := &User{
+		Username:  "",
+		Password:  "nutanix/4u",
+		DbSecrets: []string{},
+	}
+
+	empty := &User{}
+
+	userScrubbed := &User{
+		Username:  "",
+		Password:  "********",
+		DbSecrets: []string{},
+	}
+
+	// Validate input with empty fields
+	validateScrub(t, empty, user, userScrubbed, nil, XmlScrub)
+
+	// Validate empty pointer input
+	var userEmpty *User
+	validateScrub(t, empty, userEmpty, userEmpty, nil, XmlScrub)
+
+	// Validate nil input
+	validateScrub(t, empty, nil, nil, nil, XmlScrub)
+}
+
+// Tests json format scrubbing on a nested complex struct with some nil, empty and specified sensitive fields.
+func TestScrubNestedNilJson(t *testing.T) {
+	MaskLenVary = false
+
 	users := &Users{
 		Secret: "",
 		Keys:   nil,
@@ -126,6 +402,8 @@ func TestScrubNestedNil(t *testing.T) {
 			},
 		},
 	}
+
+	empty := &Users{}
 
 	userScrubbed := &Users{
 		Secret: "",
@@ -145,22 +423,93 @@ func TestScrubNestedNil(t *testing.T) {
 	}
 
 	// Test a nested struct with some empty and nil fields.
-	secretFields := map[string]bool{
-		"password": true, "keys": true, "secret": true, "dbsecrets": true}
-	validateScrub(t, users, userScrubbed, secretFields)
+	secretFields := map[string]map[string]string{
+		"password": make(map[string]string),
+		"keys": make(map[string]string),
+		"secret": make(map[string]string),
+		"dbsecrets": make(map[string]string),
+	}
+	secretFields["password"]["symbol"] = "*"
+	secretFields["keys"]["symbol"] = "."
+	secretFields["secret"]["symbol"] = "*"
+	secretFields["dbsecrets"]["symbol"] = "*"
+
+	validateScrub(t, empty, users, userScrubbed, secretFields, JsonScrub)
+}
+
+// Tests xml format scrubbing on a nested complex struct with some nil, empty and specified sensitive fields.
+func TestScrubNestedNilXml(t *testing.T) {
+	MaskLenVary = false
+
+	users := &Users{
+		Secret: "",
+		Keys:   nil,
+		UserInfo: []User{
+			{
+				Username:  "John Doe",
+				Password:  "",
+				DbSecrets: []string{"John's_db_secret_1", "John's_db_secret_2"},
+			},
+			{
+				Username:  "Jane Doe",
+				Password:  "Jane_Doe's_Password",
+				DbSecrets: []string{},
+			},
+		},
+	}
+
+	empty := &Users{}
+
+	userScrubbed := &Users{
+		Secret: "",
+		Keys:   nil,
+		UserInfo: []User{
+			{
+				Username:  "John Doe",
+				Password:  "",
+				DbSecrets: []string{"********", "********"},
+			},
+			{
+				Username:  "Jane Doe",
+				Password:  "********",
+				DbSecrets: []string{},
+			},
+		},
+	}
+
+	// Test a nested struct with some empty and nil fields.
+	secretFields := map[string]map[string]string{
+		"password": make(map[string]string),
+		"keys": make(map[string]string),
+		"secret": make(map[string]string),
+		"dbsecrets": make(map[string]string),
+	}
+	secretFields["password"]["symbol"] = "*"
+	secretFields["keys"]["symbol"] = "."
+	secretFields["secret"]["symbol"] = "*"
+	secretFields["dbsecrets"]["symbol"] = "*"
+
+	validateScrub(t, empty, users, userScrubbed, secretFields, XmlScrub)
 }
 
 // validateScrub is a helper function to validate scrubbing functionality on a struct.
-func validateScrub(t *testing.T, msg, scrubbedMsg interface{}, secretFields map[string]bool) {
+func validateScrub(t *testing.T, cloning, target, scrubbedMsg interface{}, secretFields map[string]map[string]string, dataType ScrubDataType) {
 	t.Helper()
 
 	// Get the scrubbed string from util API.
-	got := Scrub(msg, secretFields)
+	got := Scrub(cloning, target, secretFields, dataType)
 
-	// Compare it against the given scrubbed representaation.
 	var b []byte
-	b, _ = json.Marshal(scrubbedMsg)
-	want := string(b)
+	var want string
+	// Compare it against the given scrubbed representaation.
+	switch dataType {
+	case JsonScrub:
+		b, _ = json.Marshal(scrubbedMsg)
+		want = string(b)
+	case XmlScrub:
+		b, _ = xml.MarshalIndent(scrubbedMsg, "  ", "    ")
+		want = string(b)
+	}
 
 	assert.Equal(t, want, got,
 		"JSON representation mismatch after scrubbing sensitive fields")
